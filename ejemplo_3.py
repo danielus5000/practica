e7d4f7aefafa17850153f01e57e6410a529828a1
dany <USER@sistema.con>
2018-11-11 11:48:30 -0500

    mi primer commit

---
+++ ejemplo_3.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+raiz = Tk()
+
+miFrame=Frame(raiz, width = 1200, height=600)
+miFrame.pack()
+
+cuadroNombre=Entry(miFrame)
+cuadroNombre.grid(row=1,column=4)
+
+cuadroApellido=Entry(miFrame)
+cuadroApellido.grid(row=2,column=4)
+
+cuadroDireccion=Entry(miFrame)
+cuadroDireccion.grid(row=3,column=4)
+
+nombreLabel=Label(miFrame,text="Nombre")
+nombreLabel.grid(row=1,column=3)
+
+ApellidoLabel=Label(miFrame,text="Apellido")
+ApellidoLabel.grid(row=2,column=3)
+
+DireccionLabel=Label(miFrame,text="Direccion de casa")
+DireccionLabel.grid(row=3,column=3)
+
+raiz.mainloop()
